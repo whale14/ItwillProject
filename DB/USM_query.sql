b2ec78b3275ad46ca11f9e0f304378abc4501791
bab <tjdghk1119@naver.com>
2022-03-21 18:22:59 +0900

    쿼리 작성 완료!!

--- DB/USM_query.sql
+++ DB/USM_query.sql
@@ -4,28 +4,19 @@
 
 
 
-/* 내 주변 지역 지역명별 리스트 조회*/
+/* 리스트 조회(검색/ 지역+검색 /지역)*/
 -- 1. 제목에서 검색어로 찾기
 
 SELECT c.client_name, p.product_name, P.PRICE ,c.reliablity
-
 FROM REGION R, client_info C, PRODUCT P
-
 WHERE r.region_id = C.REGION_ID 
 AND C.CLIENT_ID = P.CLIENT_ID
 AND p.product_name LIKE '%고%'  
 ;
 
 
-/*지역명으로 검색*/
-SELECT c.client_name, p.product_name, P.PRICE ,c.reliablity
-FROM REGION R, client_info C, PRODUCT P
-WHERE r.region_id = C.REGION_ID 
-AND C.CLIENT_ID = P.CLIENT_ID
-AND R.REGION_ID = '서울특별시'
-;
 
-/*지역+ 검색어로 검색*/
+/*2. 지역+ 검색어로 검색*/
 SELECT c.client_name, p.product_name, P.PRICE ,c.reliablity
 FROM REGION R, client_info C, PRODUCT P
 WHERE r.region_id = C.REGION_ID 
@@ -34,20 +25,46 @@ AND p.product_name LIKE '%고%'
 AND R.REGION_ID = '서울특별시'
 ;
 
+/*3. 지역명으로 검색*/
+SELECT c.client_name, p.product_name, P.PRICE ,c.reliablity
+FROM REGION R, client_info C, PRODUCT P
+WHERE r.region_id = C.REGION_ID 
+AND C.CLIENT_ID = P.CLIENT_ID
+AND R.REGION_ID = '서울특별시'
+;
 
 
+/*상품 등록 번호 순서대로 검색*/
 SELECT PRODUCT_NAME, product_description, PRICE 
 FROM product
 WHERE product_id = 1
 ;
 
-/* 상품정보 입력 -- DB에 추가*/
-INSERT INTO PRODUCT VALUES (?,?,?,?,/*getID*/?);
+/* 판매등록 쿼리*/
+INSERT INTO PRODUCT (PRODUCT_ID, PRODUCT_NAME, PRODUCT_DESCRIPTION, CLIENT_ID, PRICE)
+        VALUES((SELECT MAX(product_id)+1 FROM product),?,?,/*getID*/,?);
 
+/*내가 올린 상품 조회 */
+SELECT *
+FROM PRODUCT
+WHERE client_id = 1076745663
+;
 
+/*업데이트 상품명 */
+UPDATE PRODUCT SET product_name = ' '
+WHERE client_id = 1076745663
+;
 
+/*업데이트 내용*/  
+UPDATE PRODUCT SET product_DESCRIPTION = ' '
+WHERE client_id = 1076745663
+;
 
+/*업데이트 금액*/  
+UPDATE PRODUCT SET PRICE = ' '
+WHERE client_id = 1076745663
+;
 
-
-
-
+/*회원탈퇴*/
+DELETE CLIENT_INFO WHERE CLIENT_ID = 1076745663;
+SELECT * FROM CLIENT_INFO;
