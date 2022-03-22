b02602970b0b59601477302d9d1795abbb9c74de
bab <tjdghks1119@naver.com>
2022-03-22 10:41:28 +0900

    usm product 파일 풀 하시면 다시 스크립트 실행해주세요

--- DB/USM.sql
+++ DB/USM.sql
@@ -3,7 +3,7 @@ CREATE TABLE CLIENT_INFO(
     CLIENT_ID NUMBER(13) PRIMARY KEY,                                    -- 전화번호
     CLIENT_PW VARCHAR2(30) NOT NULL,                                     -- 비밀번호
     CLIENT_NAME VARCHAR2(50) NOT NULL,                                   -- 회원명
-    REGIN_ID VARCHAR2(50) NOT NULL,                                      -- 지역ID
+    REGION_ID VARCHAR2(50) NOT NULL,                                      -- 지역ID
     RELIABLIITY NUMBER(3) DEFAULT 0 CHECK(RELIABLIITY BETWEEN 0 AND 100) -- 신뢰도 
 )
 ;
@@ -20,8 +20,8 @@ CREATE TABLE PRODUCT(
     PRODUCT_NAME VARCHAR2(50) NOT NULL, -- 상품명
     PRODUCT_DESCRIPTION VARCHAR(200),   -- 상품설명
     CLIENT_ID NUMBER(13) NOT NULL,      -- 회원ID
-    REGION_ID VARCHAR2(50) NOT NULL,    -- 지역ID
     PRICE NUMBER(9) NOT NULL            -- 가격
 )
 ;
 
+COMMIT;
